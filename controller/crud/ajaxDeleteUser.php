c05c8161aa95f2b4a566260f0fe185fb20b58d10
Zyanya714 <zyanya-59427@hotmail.com>
2023-04-28 10:40:34 -0600

    Changes to be committed: 	modified:   controller/crud/ajaxDeleteUser.php 	renamed:    views/img/paciente/FOTO_37.png -> views/img/paciente/FOTO_34.png 	deleted:    views/img/paciente/FOTO_39.jpg 	deleted:    views/img/paciente/FOTO_40.jpg 	new file:   views/img/paciente/FOTO_42.jpg 	renamed:    views/img/paciente/FOTO_38.png -> views/img/tera/FOTO_1.png 	renamed:    views/img/tera/FOTO_37.png -> views/img/tera/FOTO_2.png 	new file:   views/img/tera/FOTO_3.png

--- controller/crud/ajaxDeleteUser.php
+++ controller/crud/ajaxDeleteUser.php
@@ -4,15 +4,30 @@ include('../conexion.php');
 if(!empty($_POST["id"])){
         $tipo_usuario=$_POST['tu'];
         $id=$_POST['id'];
-    if($tipo_usuario=="3"){$query = "SELECT idUsuario FROM pacientes WHERE id_paciente = '".$id."'";
-    }elseif($tipo_usuario=="2"){$query = "SELECT idUsuario FROM terapeutas WHERE id_terapeuta = '".$id."'";}
+        $conexion->query("START TRANSACTION");
+    if($tipo_usuario=="3"){$query = "SELECT idUsuario,url_foto_p FROM pacientes WHERE id_paciente = '".$id."'";
+    }elseif($tipo_usuario=="2"){$query = "SELECT idUsuario,url_foto FROM terapeutas WHERE id_terapeuta = '".$id."'";}
     $res = $conexion->query($query);
     if($res == TRUE){
         $row_usuarios = $res->fetch_assoc();
         $idUsuario=$row_usuarios['idUsuario'];
+        if($tipo_usuario=="3"){$url=$row_usuarios['url_foto_p'];}elseif($tipo_usuario=="2"){$url=$row_usuarios['url_foto'];}
         $sql="DELETE FROM `usuarios` WHERE idUsuario = '".$idUsuario."'";
         $res_del = $conexion->query($sql);
-        if($res_del==TRUE){echo('success');}else{echo('Error al eliminar: '.$conexion->error);}
-    }else{echo('Error: '.$conexion->error);}
+        if($res_del==TRUE){
+            $url_complete="../../".$url;
+            if(file_exists($url_complete)){
+                if(unlink($url_complete)){
+                    $conexion->query("COMMIT");echo('success');
+                }else{
+                    $conexion->query("ROLLBACK");echo('Error se encontró el archivo pero no se pudo eliminar: '.$url);
+                }
+            }else{
+                $conexion->query("ROLLBACK");echo('Error no se encuentra el archivo: '.$url);
+            }
+        }else{
+            $conexion->query("ROLLBACK");echo('Error al eliminar: '.$conexion->error);
+        }
+    }else{$conexion->query("ROLLBACK");echo('Error: '.$conexion->error);}
 }
 ?>
